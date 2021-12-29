4520286ab57a02149bcf06d4a7b002721b6f8ed9
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2021-12-29 08:27:01 +0100

    submit comment button small

--- src/components/Post.js
+++ src/components/Post.js
@@ -193,7 +193,7 @@ function Post(props) {
                 />
               </Form.Group>
 
-              <Button variant="primary" type="submit" className="float-end">
+              <Button variant="primary" size="sm" type="submit" className="float-end">
                 Submit
               </Button>
             </Form>
