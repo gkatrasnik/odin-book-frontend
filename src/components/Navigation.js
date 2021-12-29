17d1f2d60b6ecb48b53495d48fad95b8e5c065b0
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2021-12-29 14:27:28 +0100

    navbar collapse

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ function Navigation() {
   const { user, logout } = useContext(UserContext);
 
   return (
-    <Navbar style={{ borderBottom: "1px solid black" }}>
+    <Navbar style={{ borderBottom: "1px solid black" }} expand="lg">
       <Container>
         <Navbar.Brand>
           <h3>ODIN BOOK</h3>
