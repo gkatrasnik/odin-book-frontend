a45506f5402ba4b86dc4275d3f495cd2afdbdd5e
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2021-12-29 09:55:49 +0100

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -183,7 +183,7 @@ function Post(props) {
             </Button>
           </Card.Text>
 {showCommentForm && (
-            <Form onSubmit={handleCommentAdd}>
+            <Form onSubmit={handleCommentAdd} className="bg-secondary text-white">
               <Form.Group className="mb-3" controlId="content">
                 <Form.Label>Text:</Form.Label>
                 <Form.Control
