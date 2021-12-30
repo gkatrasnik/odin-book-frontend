8993913df0ff9e36cd53943b5b04d458fc56c41e
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2021-12-30 11:24:31 +0100

    icons added to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 import { Link } from "react-router-dom";
+import { Bell, Person, PersonPlus, PersonCheck, People, Newspaper, BoxArrowRight, BoxArrowInRight, BoxArrowInUp  } from "react-bootstrap-icons";
 
 function Navigation() {
   const { user, logout } = useContext(UserContext);
@@ -23,22 +24,27 @@ function Navigation() {
             <>
               <Nav className="me-auto">
                 <Nav.Link as={Link} to="/" eventKey="1">
+                  <Newspaper/>
                   Timeline
                 </Nav.Link>
 
                 <Nav.Link as={Link} to="friendlist" eventKey="2">
+                  <People/>
                   My Friends
                 </Nav.Link>
 
                 <Nav.Link as={Link} to="friendsugestions" eventKey="3">
+                  <PersonPlus/>
                   Add Friends
                 </Nav.Link>
 
                 <Nav.Link as={Link} to="friendrequests" eventKey="5">
+                  <PersonCheck/>
                   Friend Requests
                 </Nav.Link>
 
                 <Nav.Link as={Link} to="notifications" eventKey="4">
+                  <Bell/>
                   Notifications
                 </Nav.Link>
               </Nav>
@@ -49,11 +55,12 @@ function Navigation() {
                   state={{ targetUser: user }}
                   className="text-primary"
                   eventKey="6"
-                >
+                ><Person/>
                   {user.firstname} {user.lastname}
                 </Nav.Link>
 
                 <Nav.Link onClick={logout} className="text-danger" eventKey="7">
+                  <BoxArrowRight/>
                   Logout
                 </Nav.Link>
               </Nav>
@@ -61,10 +68,12 @@ function Navigation() {
           ) : (
             <Nav>
               <Nav.Link as={Link} to="login" eventKey="8">
+                <BoxArrowInRight/>
                 Login
               </Nav.Link>
 
               <Nav.Link as={Link} to="signup" eventKey="9">
+                <BoxArrowInUp/>
                 Sign up
               </Nav.Link>
             </Nav>
