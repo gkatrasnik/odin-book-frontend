53fb23838d28b1eff9d4a5a76ab550e7f2ae8e76
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-12-31 18:26:20 +0100

    notifications and friend request count added to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,65 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { useContext } from "react";
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 import { Link } from "react-router-dom";
-import { Bell, Person, PersonPlus, PersonCheck, People, Newspaper, BoxArrowRight, BoxArrowInRight, BoxArrowInUp  } from "react-bootstrap-icons";
+import {
+  Bell,
+  Person,
+  PersonPlus,
+  PersonCheck,
+  People,
+  Newspaper,
+  BoxArrowRight,
+  BoxArrowInRight,
+  BoxArrowInUp,
+} from "react-bootstrap-icons";
+import axios from "axios";
 
 function Navigation() {
   const { user, logout } = useContext(UserContext);
+  const [notificationsList, setNotificationsList] = useState([]);
+  const [friendRequestsList, setFriendRequestsList] = useState([]);
+
+  useEffect(() => {
+    getNotificationsData();
+    getFriendRequestsData();
+  }, [user]);
+
+  const getNotificationsData = async () => {
+    const userId = user._id;
+    const token = localStorage.getItem("token");
+
+    axios
+      .get(
+        `/api/users/${userId}/notifications`,
+
+        {
+          headers: { Authorization: "Bearer " + token },
+        }
+      )
+      .then((response) => {
+        setNotificationsList(response.data.user.notifications);
+      })
+      .catch((err) => {});
+  };
+
+  const getFriendRequestsData = async () => {
+    const userId = user._id;
+    const token = localStorage.getItem("token");
+
+    axios
+      .get(
+        `/api/users/${userId}/notifications`,
+
+        {
+          headers: { Authorization: "Bearer " + token },
+        }
+      )
+      .then((response) => {
+        setFriendRequestsList(response.data.user.recieved_friend_requests);
+      })
+      .catch((err) => {});
+  };
 
   return (
     <Navbar
@@ -15,8 +69,8 @@ function Navigation() {
     >
       <Container>
         <Navbar.Brand>
-        <Nav.Link as={Link} to="/" >
-          <h3>FAKE BOOK</h3>
+          <Nav.Link as={Link} to="/">
+            <h3>FAKE BOOK</h3>
           </Nav.Link>
         </Navbar.Brand>
 
@@ -26,28 +80,33 @@ function Navigation() {
             <>
               <Nav className="me-auto">
                 <Nav.Link as={Link} to="/" eventKey="1">
-                  <Newspaper/>{" "}
-                  Timeline
+                  <Newspaper /> Timeline
                 </Nav.Link>
 
                 <Nav.Link as={Link} to="/friendlist" eventKey="2">
-                  <People/>{" "}
-                  My Friends
+                  <People /> My Friends
                 </Nav.Link>
 
                 <Nav.Link as={Link} to="/friendsugestions" eventKey="3">
-                  <PersonPlus/>{" "}
-                  Add Friends
+                  <PersonPlus /> Add Friends
                 </Nav.Link>
 
                 <Nav.Link as={Link} to="/friendrequests" eventKey="5">
-                  <PersonCheck/>{" "}
-                  Friend Requests
+                  <PersonCheck /> Friend Requests{" "}
+                  {friendRequestsList.length > 0 && (
+                    <Badge pill bg="danger">
+                      {friendRequestsList.length}
+                    </Badge>
+                  )}
                 </Nav.Link>
 
                 <Nav.Link as={Link} to="/notifications" eventKey="4">
-                  <Bell/>{" "}
-                  Notifications
+                  <Bell /> Notifications{" "}
+                  {notificationsList.length > 0 && (
+                    <Badge pill bg="danger">
+                      {notificationsList.length}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </Nav>
               <Nav>
@@ -57,26 +116,23 @@ function Navigation() {
                   state={{ targetUser: user }}
                   className="text-primary"
                   eventKey="6"
-                ><Person/>{" "}
-                  {user.firstname} {user.lastname}
+                >
+                  <Person /> {user.firstname} {user.lastname}
                 </Nav.Link>
 
                 <Nav.Link onClick={logout} className="text-danger" eventKey="7">
-                  <BoxArrowRight/>{" "}
-                  Logout
+                  <BoxArrowRight /> Logout
                 </Nav.Link>
               </Nav>
             </>
           ) : (
             <Nav>
               <Nav.Link as={Link} to="/login" eventKey="8">
-                <BoxArrowInRight/>{" "}
-                Login
+                <BoxArrowInRight /> Login
               </Nav.Link>
 
               <Nav.Link as={Link} to="/signup" eventKey="9">
-                <BoxArrowInUp/>{" "}
-                Sign up
+                <BoxArrowInUp /> Sign up
               </Nav.Link>
             </Nav>
           )}
